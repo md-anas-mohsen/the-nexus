6c7b4e42b95a3db0b196964106e828d66ca82b1d
Anas Mohsen <anas.mohsen65@gmail.com>
2026-01-21 01:53:15 +0500

    case sensitve StarFall error

--- src/components/services/atmosphere/Atmosphere.jsx
+++ src/components/services/atmosphere/Atmosphere.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Nebula from "../../hero/atmosphere/nebula/Nebula";
-import StarField from "./starfield/starfield";
+import StarField from "./starfield/StarField";
 
 const Atmosphere = () => {
   return (
