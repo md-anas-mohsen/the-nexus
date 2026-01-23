cefb2e8481d3342a45dfb44a553fa7fb0aadc292
Anas Mohsen <anas.mohsen65@gmail.com>
2026-01-24 01:53:32 +0500

    WIP mark

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -375,7 +375,7 @@ const Portfolio = () => {
   return (
     <div className="portfolio" ref={ref}>
       <div className="progress">
-        <h1>Featured Works</h1>
+        <h1>Featured Works (Work in progress)</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
       {items.map((item) => (
